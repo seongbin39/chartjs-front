722d4011d33178330bad34ddb55887245a523d92
seongbin3931 <seongbin3931@gmail.com>
2021-12-03 15:23:30 +0900

    Layout 수정

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,16 @@
 function Layout(props){
-    const {children} = this
+    const { children } = props
     const layoutStyle = {
         display: "grid",
         gridTemplateColumns: "1fr 1fr",
         border: "1px solid rgba(232, 232, 232, 0.7)"
     }
-    const myChildren = children.map((child) =>{
-        const child = {
+    const myChildren = children.map( (child) =>{
+        const childs = {
             border: "1px solid rgba(232, 232, 232, 1)"
         }
         return (
-            <div>
+            <div style={childs}>
                 {child}
             </div>
         )
