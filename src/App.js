0cb633023a3914acb84d8ac67645d3b744174a04
seongbin3931 <seongbin3931@gmail.com>
2021-12-03 14:34:13 +0900

    data 전달구조 리팩토링

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react'
 import VerticalBarChart from './components/VerticalBarChart'
 import HorizontalBarChart from "./components/HorizontalBarChart"
 import LineChart from "./components/LineChart"
+import StackedBarChart from "./components/StackedBarChart"
+import StackedBarChartWithGroups from "./components/StackedBarChartWithGroups"
 import Layout from './components/Layout'
 
 function App() {
+  const data = [50, 40, 30, 35, 40]
+  const labels = [2017, 2018, 2019, 2020 ,2021]
   return (
     <Layout>
-      <VerticalBarChart/>
-      <HorizontalBarChart/>
-      <LineChart/>
+      <VerticalBarChart data={data} labels={labels}/>
+      <HorizontalBarChart data={data} labels={labels}/>
+      <LineChart data={data} labels={labels}/>
     </Layout>
   );
 }
